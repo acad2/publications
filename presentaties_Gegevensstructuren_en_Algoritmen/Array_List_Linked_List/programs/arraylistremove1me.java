c005bbfd16f58cce1824ef70d18c1622bc5ee6d3
KommuSoft <vanonsem.willem@gmail.com>
2013-03-31 18:56:55 +0200

    ArrayList operators finished

---
+++ presentaties_Gegevensstructuren_en_Algoritmen/Array_List_Linked_List/programs/arraylistremove1me.java
@@ -0,0 +1,10 @@
+
+ArrayList<String> namen = new ArrayList<String>();
+namen.add("Samson");
+namen.add("Gert");
+namen.add("Octaaf");
+namen.add(2,"Alberto");
+String verw = namen.remove(1);
+verw = namen.remove(2);
+boolean wasin = namen.remove("Burgemeester");
+wasin = namen.remove("Alberto");
