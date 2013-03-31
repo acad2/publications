94aa3cc4f60e2b0ff5abcb10d118d649395be09d
KommuSoft <vanonsem.willem@gmail.com>
2013-03-31 18:25:14 +0200

    with add0, add1, remove0

---
+++ presentaties_Gegevensstructuren_en_Algoritmen/Array_List_Linked_List/programs/arraylistremove0me.java
@@ -0,0 +1,8 @@
+
+ArrayList<String> namen = new ArrayList<String>();
+namen.add("Samson");
+namen.add("Gert");
+namen.add("Octaaf");
+namen.add(2,"Alberto");
+String verw = namen.remove(1);
+verw = namen.remove(2);
