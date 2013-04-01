032ffb7db3a776627536174c3b264bc182844300
KommuSoft <vanonsem.willem@gmail.com>
2013-04-01 06:01:16 +0200

    now with animation I

--- presentaties_Gegevensstructuren_en_Algoritmen/Array_List_Linked_List/programs/arraylistsetcapaciteit.java
+++ presentaties_Gegevensstructuren_en_Algoritmen/Array_List_Linked_List/programs/arraylistsetcapaciteit.java
@@ -2,13 +2,11 @@ private void setCapaciteit (int cap) {
   if(cap < aantal) {
     throw new IllegalArgumentException();
   }
-  if(cap != elementen.length) {
-    Object[] nieuw = new Object[cap];
-    Object temp;
-    for(int i = 0; i < aantal; i++) {
-      temp = elementen[i];
-      nieuw[i] = temp;
-    }
-    elementen = nieuw;
+  Object[] nieuw = new Object[cap];
+  Object temp;
+  for(int i = 0; i < aantal; i++) {
+    temp = elementen[i];
+    nieuw[i] = temp;
   }
+  elementen = nieuw;
 }
