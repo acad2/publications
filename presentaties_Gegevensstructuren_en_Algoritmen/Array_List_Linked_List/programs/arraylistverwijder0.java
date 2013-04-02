db329b3474d6888cf3ca5a5319aa7e6a69a7fc9e
KommuSoft <vanonsem.willem@gmail.com>
2013-04-02 21:22:04 +0200

    working on remove0

---
+++ presentaties_Gegevensstructuren_en_Algoritmen/Array_List_Linked_List/programs/arraylistverwijder0.java
@@ -0,0 +1,13 @@
+private Object verwijder (int index) {
+  if(index < 0 || index >= aantal) {
+  	throw new IndexOutOfBoundsException();
+  }
+  Object outp = elementen[index];
+  Object temp;
+  for(int i = index; i < aantal-1; i++) {
+  	temp = elementen[i+1];
+  	elementen[i] = temp;
+  }
+  elementen[aantal-1] = null;
+  this.aantal--;
+}
