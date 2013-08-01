5970c774525973ae87071c02e0ae7b538ada9d50
KommuSoft <vanonsem.willem@gmail.com>
2013-08-01 17:59:08 +0200

    initial work on min-bus

--- cursus_Digitale_Elektronica_en_Processoren/programs/code.hs
+++ cursus_Digitale_Elektronica_en_Processoren/programs/code.hs
@@ -2,9 +2,17 @@ enrich :: [x] -> (x -> y) -> [(x,y)]
 enrich [] _ = []
 enrich (x:xs) f = ((x,f x) : enrich xs f)
 
-calculatePartition :: [v] -> [([v],l)] -> [[v]]
+calculatePartition :: (Ord l, Eq v) => [v] -> [([v],l)] -> [[v]]
 calculatePartition x [] = map (\a -> [a]) x
-calculatePartition x y = (vs:) where (vs,ls) = argmax y
+calculatePartition x y = (vs:calculatePartition vr er) where (vs,ls) = argmax y
+                                                             vr = setminus x vs
+                                                             er = significantEdges y vr
+
+setminus :: (Eq a) => [a] -> [a] -> [a]
+setminus a b = [x|x <- a, notElem x b]
+
+significantEdges :: (Eq a) => [([a],b)] -> [a] -> [([a],b)]
+significantEdges e c = [(x,y)|(x,y)<-e,all (\z -> elem z c) x]
 
 argmax :: (Ord y) => [(x,y)] -> (x,y)
 argmax (x:xs) = argmax' x xs
