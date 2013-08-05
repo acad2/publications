aeea792b5200252644f7c8c6dfaa5e0128a43eba
KommuSoft <vanonsem.willem@gmail.com>
2013-08-05 22:03:07 +0200

    modified comparison table

--- cursus_Digitale_Elektronica_en_Processoren/programs/program.hs
+++ cursus_Digitale_Elektronica_en_Processoren/programs/program.hs
@@ -1,2 +1,7 @@
-type Instruction instruction register = (register,instruction,[register])
-type InstructionBlock instruction register = [Instruction instruction register]
+type Ins ins reg = (reg,ins,[reg])
+type InsCol ins reg = [Ins ins reg]
+
+data Comp = eq | st | lt | lte | ste
+data BoolOp = and | or | not
+data RegVal reg val = Reg reg | Val val
+data Condition = Cond BoolOp Condition Condition | Cond 
