4077d87140db0c45d3af2209bad02da913fbaab8
KommuSoft <vanonsem.willem@gmail.com>
2013-12-01 03:13:18 +0100

    merging programs

--- cursus_Digitale_Elektronica_en_Processoren/programs/program.hs
+++
@@ -1,7 +0,0 @@
-type Ins ins reg = (reg,ins,[reg])
-type InsCol ins reg = [Ins ins reg]
-
-data Comp = eq | st | lt | lte | ste
-data BoolOp = and | or | not
-data RegVal reg val = Reg reg | Val val
-data Condition = Cond BoolOp Condition Condition | Cond 
