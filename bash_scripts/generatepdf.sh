d7aede7b8a181c147333b90ec822591ba778d215
KommuSoft <vanonsem.willem@gmail.com>
2014-03-23 01:37:19 +0100

    updated readme

--- bash_scripts/generatepdf.sh
+++ bash_scripts/generatepdf.sh
@@ -1,13 +1,13 @@
 bnm=$(basename "$1" ".pdf")
 bnm=$(basename "$bnm" ".tex")
 pdflatex --shell-escape --interaction nonstopmode "$bnm.tex" || true
-makeglossaries -q "$bnm"
-makeindex -q "$bnm"
-pdflatex --shell-escape --interaction nonstopmode "$bnm.tex"
-bibtex "$bnm"
-makeglossaries -q "$bnm"
-makeindex -q "$bnm"
-pdflatex --shell-escape --interaction nonstopmode "$bnm.tex"
-bibtex "$bnm"
-pdflatex --shell-escape --interaction nonstopmode "$bnm.tex"
-pdflatex --shell-escape --interaction nonstopmode "$bnm.tex"
+makeglossaries -q "$bnm" || true
+makeindex -q "$bnm" || true
+pdflatex --shell-escape --interaction nonstopmode "$bnm.tex" || true
+bibtex "$bnm" || true
+makeglossaries -q "$bnm" || true
+makeindex -q "$bnm" || true
+pdflatex --shell-escape --interaction nonstopmode "$bnm.tex" || true
+bibtex "$bnm" || true
+pdflatex --shell-escape --interaction nonstopmode "$bnm.tex" || true
+pdflatex --shell-escape --interaction nonstopmode "$bnm.tex" || true
