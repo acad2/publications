5d37ff9e6b69f085a7504e92298b2e658e19eb90
KommuSoft <vanonsem.willem@gmail.com>
2014-04-09 21:41:36 +0200

    more gitignore

--- belgian_crime_dictionary/distribute.sh
+++ belgian_crime_dictionary/distribute.sh
@@ -1,9 +1,9 @@
 while read p; do
-	grep "\\individual{$p" < all.tex > "$p.tex"
-	grep "\\evententry{$p" < all.tex >> "$p.tex"
-	grep "\\caseentry{$p" < all.tex >> "$p.tex"
-	grep "\\groupentry{$p" < all.tex >> "$p.tex"
-	grep "\\commissieentry{$p" < all.tex >> "$p.tex"
-	grep "\\operationentry{$p" < all.tex >> "$p.tex"
+	grep "\\individual{$p" < all.tex > "$p.texp"
+	grep "\\evententry{$p" < all.tex >> "$p.texp"
+	grep "\\caseentry{$p" < all.tex >> "$p.texp"
+	grep "\\groupentry{$p" < all.tex >> "$p.texp"
+	grep "\\commissieentry{$p" < all.tex >> "$p.texp"
+	grep "\\operationentry{$p" < all.tex >> "$p.texp"
 done < chaps
-grep "\\annon{" < all.tex > "annon.tex"
+grep "\\annon{" < all.tex > "annon.texp"
