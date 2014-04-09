c0d97edcb6d6e14dd24e9a75c1ec3e353f217c14
KommuSoft <vanonsem.willem@gmail.com>
2014-04-09 21:53:20 +0200

    better makefile

---
+++ belgian_crime_dictionary/makepdf.sh
@@ -0,0 +1,12 @@
+pdflatex -interaction nonstopmode records.tex > /dev/null 2>/dev/null || true
+pdflatex -interaction nonstopmode records.tex > /dev/null 2>/dev/null || true
+bibtex records > /dev/null 2>/dev/null || true
+makeindex records > /dev/null 2>/dev/null || true
+makeglossaries records > /dev/null 2>/dev/null || true
+pdflatex -interaction nonstopmode records.tex > /dev/null 2>/dev/null || true
+pdflatex -interaction nonstopmode records.tex > /dev/null 2>/dev/null || true
+bibtex records > /dev/null 2>/dev/null || true
+makeindex records > /dev/null 2>/dev/null || true
+makeglossaries records > /dev/null 2>/dev/null || true
+pdflatex -interaction nonstopmode records.tex > /dev/null 2>/dev/null || true
+pdflatex -interaction nonstopmode records.tex > /dev/null 2>/dev/null || true
