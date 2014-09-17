8b2cb93fd697457a5ef1d1dcc812af0630980800
KommuSoft <vanonsem.willem@gmail.com>
2014-09-18 01:00:01 +0200

    more structural code style

--- programmingcontests/hs/honeycomb.hs
+++ programmingcontests/hs/honeycomb.hs
@@ -5,6 +5,6 @@ hexmoves (x,y) | even (x + y) == even x = [(x+1,y),(x,y-1),(x-1,y+1)]
                | otherwise = [(x-1,y),(x,y+1),(x+1,y-1)]
 
 dijkstra :: (Eq a,Num b,Ord b,Bounded b) => [a] -> [a] -> (a->([a],b)) -> ([a],b)
-dijkstra s d f = dijkstra'
+dijkstra s d f = dijkstra' (\x -> (x,minbound )) s
 
---dijkstra' :: (Eq a,Num b,Ord b,Bounded b) => [([a],b)] -> [a] -> (a->([a],b)) -> ([a],b)
+--dijkstra' :: (Eq a,Num b,Ord b,Bounded b) => [(a,b)] -> [a] -> (a->([a],b)) -> ([a],b)
---
+++ programmingcontests/inputproblems.sh
@@ -0,0 +1,1 @@
+ls problems/*.tex | xargs printf "\input{%s}\n" > problems.tex
