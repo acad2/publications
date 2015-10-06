8e309b5dfbe13397ac0f288d81f43effaaa8fb64
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-06 15:33:09 +0200

    better table creation

--- cursus_Digitale_Elektronica_en_Processoren/ctable.sh
+++ cursus_Digitale_Elektronica_en_Processoren/ctable.sh
@@ -7,18 +7,19 @@ pref="tables/tbl-"
 #Check if a label is provided
 if [ "$#" -le 0 ]
 then
-    echo "Please provide a parameter to label the table!"
-    exit 1
+    read -p "Please provide a parameter to label the table!" -r lbl
+else
+    lbl="$1"
 fi
 
 #Check if a table with the label already exists
-if [ -f "$pref$1.tex" ]
+if [ -f "$pref$lbl.tex" ]
 then
-    echo "The table with the label \"$1\" already exists!"
+    echo "The table with the label \"$lbl\" already exists!"
     exit 2
 fi
 
 #Copy the template and start the editor
-cp "tbl/tbl-template.tex" "tbl/tbl-$1.tex"
-vim "tbl/tbl-$1.tex"
+cp "tbl/tbl-template.tex" "tbl/tbl-$lbl.tex"
+vim "tbl/tbl-$lbl.tex"
 exit 0
