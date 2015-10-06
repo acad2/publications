b9b6472a0972134076002e95f104dd53f2faa970
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-07 00:36:28 +0200

    better compile loop script

--- cursus_Digitale_Elektronica_en_Processoren/compileLoopScript.sh
+++ cursus_Digitale_Elektronica_en_Processoren/compileLoopScript.sh
@@ -6,6 +6,15 @@ renice -n 19 -p "$$"
 
 while true
 do
-    make
-    sleep 60
+    for f in `seq 20`
+    do
+        make
+        read -p 'press "q" to quit' -t 60 -n 1 -r a
+        echo
+        if [ "$a" == "q" ]
+        then
+            exit 0
+        fi
+    done
+    make clean
 done
