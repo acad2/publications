2101408eb2d9962dea09151f4403329e745083e9
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-10 01:03:20 +0200

    better sleeppdf script

--- generatepdfsleep.sh
+++ generatepdfsleep.sh
@@ -1,15 +1,26 @@
-timeout 600 pdflatex --interaction nonstopmode "$1.tex" || true
-timeout 600 makeglossaries -q "$1"
-timeout 600 makeindex -q "$1"
-sleep 1
-timeout 600 pdflatex --interaction nonstopmode "$1.tex"
-timeout 600 bibtex "$1"
-timeout 600 makeglossaries -q "$1"
-timeout 600 makeindex -q "$1"
-sleep 1
-timeout 600 pdflatex --interaction nonstopmode "$1.tex"
-timeout 600 bibtex "$1"
-sleep 1
-timeout 600 pdflatex --interaction nonstopmode "$1.tex"
-sleep 1
-timeout 600 pdflatex --interaction nonstopmode "$1.tex"
+#!/bin/bash
+
+tmo=900            #15 minutes, the maximum amount of time the compilers get per session
+                   #to ensure this script will always and pdflatex won't get it in an infinite loop
+tim=1              #1 second, the amount of time between two compilation sequences
+pint="nonstopmode" #The interaction mode used for pdflatex
+
+timeout $tmo pdflatex --interaction $pnt "$1.tex" || true
+timeout $tmo makeglossaries -q "$1"
+timeout $tmo makeindex -q "$1"
+sleep $tim
+
+timeout $tmo pdflatex --interaction $pnt "$1.tex"
+timeout $tmo bibtex "$1"
+timeout $tmo makeglossaries -q "$1"
+timeout $tmo makeindex -q "$1"
+sleep $tim
+
+timeout $tmo pdflatex --interaction $pnt "$1.tex"
+timeout $tmo bibtex "$1"
+sleep $tim
+
+timeout $tmo pdflatex --interaction $pnt "$1.tex"
+sleep $tim
+
+timeout $tmo pdflatex --interaction $pnt "$1.tex"
