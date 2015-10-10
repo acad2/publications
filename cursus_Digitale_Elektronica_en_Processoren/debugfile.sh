5835a450a3d1f5c5e6367207706a395593aff233
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-10 22:44:20 +0200

    temporary commit

--- cursus_Digitale_Elektronica_en_Processoren/debugfile.sh
+++ cursus_Digitale_Elektronica_en_Processoren/debugfile.sh
@@ -8,10 +8,11 @@ for lb in `grep -h -n "^\.\/$1:[0-9]*:" "$lgf" | cut -d':' -f 1`
 do
     tail -n "+$lb" "$lgf" | grep -m 1 '^l\.' -B 999
     read -t $tmo -p 'press "e" to edit; wait or press any key to continue? ' -n 1 -r a
+    echo
     if [[ "$a" =~ [eE] ]]
     then
         la=$(tail -n "+$lb" "$lgf" | grep -m 1 '^l\.' | grep -m 1 '[0-9]*' -o)
         vim +"$la" "$fle"
     fi
 done
-
+git commit -am "debug \"$fle\""
