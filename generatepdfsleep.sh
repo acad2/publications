36b5da97e029a328d1fc9e2a7e489307e52c08a9
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-10 02:49:45 +0200

    fixed script typo

--- generatepdfsleep.sh
+++ generatepdfsleep.sh
@@ -5,22 +5,22 @@ tmo=900            #15 minutes, the maximum amount of time the compilers get per
 tim=1              #1 second, the amount of time between two compilation sequences
 pint="nonstopmode" #The interaction mode used for pdflatex
 
-timeout $tmo pdflatex --interaction $pnt "$1.tex" || true
+timeout $tmo pdflatex --interaction $pint "$1.tex" || true
 timeout $tmo makeglossaries -q "$1"
 timeout $tmo makeindex -q "$1"
 sleep $tim
 
-timeout $tmo pdflatex --interaction $pnt "$1.tex"
+timeout $tmo pdflatex --interaction $pint "$1.tex"
 timeout $tmo bibtex "$1"
 timeout $tmo makeglossaries -q "$1"
 timeout $tmo makeindex -q "$1"
 sleep $tim
 
-timeout $tmo pdflatex --interaction $pnt "$1.tex"
+timeout $tmo pdflatex --interaction $pint "$1.tex"
 timeout $tmo bibtex "$1"
 sleep $tim
 
-timeout $tmo pdflatex --interaction $pnt "$1.tex"
+timeout $tmo pdflatex --interaction $pint "$1.tex"
 sleep $tim
 
-timeout $tmo pdflatex --interaction $pnt "$1.tex"
+timeout $tmo pdflatex --interaction $pint "$1.tex"
