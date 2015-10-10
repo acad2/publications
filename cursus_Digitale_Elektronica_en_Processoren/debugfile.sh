ab7ca253b3006ec6ec534157b41a7adeedd8084b
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-10 17:46:47 +0200

    debugfile script works

--- cursus_Digitale_Elektronica_en_Processoren/debugfile.sh
+++ cursus_Digitale_Elektronica_en_Processoren/debugfile.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+lgf="logger.dat"
 fle="$1"
+tmo=120
 
-for lb in `grep -h -n "^\.\/$1:[0-9]*:" "logger.dat" | cut -d':' -f 1`
+for lb in `grep -h -n "^\.\/$1:[0-9]*:" "$lgf" | cut -d':' -f 1`
 do
-   
+    tail -n "+$lb" "$lgf" | grep -m 1 '^l\.' -B 999
+    read -t $tmo -p 'press "e" to edit; wait or press any key to continue? ' -n 1 -r a
+    if [[ "$a" =~ [eE] ]]
+    then
+        la=$(tail -n "+$lb" "$lgf" | grep -m 1 '^l\.' | grep -m 1 '[0-9]*' -o)
+        vim +"$la" "$fle"
+    fi
 done
 
