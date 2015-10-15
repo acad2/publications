30d5e05a82e4f4871b995e9f6617980ff6cc3e3c
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-15 16:09:51 +0200

    temporary commit

--- cursus_Digitale_Elektronica_en_Processoren/compileLoopScript.sh
+++ cursus_Digitale_Elektronica_en_Processoren/compileLoopScript.sh
@@ -2,23 +2,29 @@
 git checkout dep >/dev/null 2>/dev/null
 cd "`dirname $0`"
 
+pdff="cursus.pdf"
+pdfu="ulyssis:www/cursus_dep.pdf"
+
 renice -n 19 -p "$$" >/dev/null 2>/dev/null
 
 while true
 do
     for f in `seq 20`
     do
-        make cursus.pdf >/dev/null 2>/dev/null
-        read -t 60 -p 'press "q" to quit; any other key to continue ' -n 1 -r a
+        make "$pdff" >/dev/null 2>/dev/null
+        read -t 60 -p 'press "q" to quit; "u" to upload; any other key to continue ' -n 1 -r a
         echo ""
-        if [[ "$a" =~ [qQ] ]]
+        if [[ "$a" =~ [uU] ]]
         then
-            fs=$(wc -c cursus.pdf | cut -d' ' -f 1) #obtain the file size
+            scp "$pdff" "$pdfu"
+        elif [[ "$a" =~ [qQ] ]]
+        then
+            fs=$(wc -c "$pdff" | cut -d' ' -f 1) #obtain the file size
             if [ "$fs" -gt "2000000" ] #if larger than 2 MB
             then
                 make purge
-                make cursus.pdf >/dev/null 2>/dev/null
-                scp cursus.pdf ulyssis:www/cursus_dep.pdf
+                make "$pdff" >/dev/null 2>/dev/null
+                scp "$pdff" "$pdfu"
             fi
             exit 0
         fi
