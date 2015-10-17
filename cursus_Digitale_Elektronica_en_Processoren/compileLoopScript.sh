04df89ae73bad8a4240c0094c92dd3df1cc754b0
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-10-17 15:53:20 +0200

    updated make file and more equidistant stats and commits

--- cursus_Digitale_Elektronica_en_Processoren/compileLoopScript.sh
+++ cursus_Digitale_Elektronica_en_Processoren/compileLoopScript.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+
 git checkout dep >/dev/null 2>/dev/null
 cd "`dirname $0`"
 
+#Time management
+dtz=14400 #Commit and stats every four hours
+
+#Files and servers
 pdff="cursus.pdf"
 pdfu="ulyssis:www/cursus_dep.pdf"
+stdo="logger.dat"
+stde="errors.dat"
+stds="stats.stat"
+prfx="old-"
+
+#Initial time
+tmz=$(date +%s)
+tmio=0
+tmi=0
 
 renice -n 19 -p "$$" >/dev/null 2>/dev/null
 
 while true
 do
-    for f in `seq 20`
+    while [ "$tmi" -le "$tmo" ]
     do
-        make "$pdff" >/dev/null 2>/dev/null
+        make "$pdff" >"$stdo" 2>"$stde"
+        cp "$stdo" "$prfx$stdo"
+        cp "$stde" "$prfx$stde"
+
         read -t 60 -p 'press "q" to quit; "u" to upload; any other key to continue ' -n 1 -r a
         echo ""
+
         if [[ "$a" =~ [uU] ]]
         then
             scp "$pdff" "$pdfu"
@@ -23,13 +41,20 @@ do
             if [ "$fs" -gt "2000000" ] #if larger than 2 MB
             then
                 make purge
-                make "$pdff" >/dev/null 2>/dev/null
+                make "$pdff" >"$stde" 2>"$stde"
                 scp "$pdff" "$pdfu"
             fi
             exit 0
         fi
+
+    tmn=$(date +%s)
+    tmi=$((($tmn-$tmz)/$dtz))
+
     done
-    bash generate-stats.sh >> stats.stat
+
+    tmio="$tmi"
+
+    bash generate-stats.sh >> "$stds"
     make purge >/dev/null 2>/dev/null
     git commit -am 'temporary commit'
 done
