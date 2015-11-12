1ba41ce854d08a589b10489d60192f53370142bd
Willem Van Onsem <vanonsem.willem@gmail.com>
2015-11-12 22:23:31 +0100

    debug "chapter_combinatorische_schakelingen.tex"

--- cursus_Digitale_Elektronica_en_Processoren/debugfile.sh
+++ cursus_Digitale_Elektronica_en_Processoren/debugfile.sh
@@ -8,7 +8,7 @@ while [ "$#" -gt 0 ]
     fle="$1"
     shift
     do
-    for lb in `grep -h -n "^\.\/$1:[0-9]*:" "$lgf" | cut -d':' -f 1 | tac`
+    for lb in `grep -h -n -P "^.{0,3}/$1:[0-9]*:" "$lgf" | cut -d':' -f 1 | tac`
         do
         tail -n "+$lb" "$lgf" | grep -m 1 '^l\.' -B 999
         read -t $tmo -p 'press "e" to edit; "q" to quit debugging this file; wait or press any key to continue? ' -n 1 -r a
