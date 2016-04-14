659f4feefd599b8d19598393ce5d27a75a120836
Willem Van Onsem <vanonsem.willem@gmail.com>
2016-04-14 20:33:16 +0200

    temporary commit

--- cursus_Digitale_Elektronica_en_Processoren/continue-review.sh
+++ cursus_Digitale_Elektronica_en_Processoren/continue-review.sh
@@ -11,7 +11,7 @@ done
 
 renice -n -10 -p "$$" >/dev/null 2>/dev/null
 
-for rms in "`grep -H -n -P "%\\s*$mark" *.tex | tac`" #A tribute to Richard Matthew Stallman
+for rms in "`grep -H -n -P "%\\s*$mark" *.tex`" #A tribute to Richard Matthew Stallman
   do
   fl=$(cut -d"$delm" -f 1 <<<$rms)
   ln=$(cut -d"$delm" -f 2 <<<$rms)
