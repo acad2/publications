1247e8ded17ad8603536c8e8f0e54d90b927afd3
Willem Van Onsem <vanonsem.willem@gmail.com>
2016-05-29 11:15:23 +0200

    debug "chapter_sequentiele_schakelingen.tex"

--- cursus_Digitale_Elektronica_en_Processoren/generate-stats.sh
+++ cursus_Digitale_Elektronica_en_Processoren/generate-stats.sh
@@ -18,4 +18,5 @@ fls=$(grep -m 1 -i '^filesize' $erf | grep -o -P '\d*')
 
 pgs=$(grep -m 1 -i 'pages' old-errors.dat | grep -P '\d*' -o)
 
+echo >&2 -e "timestamp\t#errors\t#warnings\t#references\t#glos\tcompile-time\tfilesize\tpages"
 echo -e "$tim\t$err\t$wrn\t$msl\t$gla\t$prd\t$fls\t$pgs"
